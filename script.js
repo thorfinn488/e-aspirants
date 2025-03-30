2c829996a904ab46356f7bd9fbd634232103c77b
YASH PAL <yashpal@HUNAR.local>
2025-03-30 13:45:10 +0530

    frontend N-theme

--- script.js
+++ script.js
@@ -82,3 +82,28 @@ slideshowContainer.addEventListener('mouseenter', () => {
 slideshowContainer.addEventListener('mouseleave', () => {
     autoSlideInterval = setInterval(() => changeSlide(1), 2000);
 });
+
+document.getElementById("cart-btn").addEventListener("click", function() {
+    window.location.href = "cart.html"; // Redirect to cart page
+});
+
+/* move the particle */
+if (pJS.particles.move.enable) {
+    var ms = pJS.particles.move.speed / 2;
+
+    if (i % 3 == 0) {
+        //sphere
+        var point = vec3.fromValues(points[i].x, points[i].y, points[i].z);
+
+        var localPoint = vec3.create();
+        vec3.transformMat4(localPoint, point, m);
+        var screenPoint = vec3.create();
+        vec3.transformMat4(screenPoint, point, pvm);
+
+        p.x = screenPoint[0] * halfWidth + pJS.canvas.w / 2;
+        p.y = screenPoint[1] * halfHeight + pJS.canvas.h / 2;
+    } else {
+        p.x += p.vx * ms;
+        p.y += p.vy * ms;
+    }
+}
